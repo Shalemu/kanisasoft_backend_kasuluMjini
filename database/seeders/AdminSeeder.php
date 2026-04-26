e009118c0b3e57a86f6814b2d383c1f24a21308a
jnatnokoi <jonathanpeter795@gmail.com>
2026-04-26 03:54:26 +0300

    Update AdminSeeder with production admin credentials

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -11,22 +11,22 @@ class AdminSeeder extends Seeder
     public function run(): void
     {
         User::updateOrCreate(
-    ['email' => 'admin@kanisasoft.com'],
-    [
-        'full_name' => 'System Admin',
-        'gender' => 'M',
-        'birth_date' => '1990-01-01',
-        'birth_place' => 'Dar es Salaam',
-        'marital_status' => 'Hajaoa',
-        'spouse_name' => null,
-        'children_count' => 0,
-        'zone' => 'Admin',
-        'phone' => '0700000000',
-        'email' => 'admin@kanisasoft.com',
-        'password' => Hash::make('admin1234'),
-        'role' => 'admin',
-    ]
-);
+            ['email' => 'lutufyo28@gmail.com'],
+            [
+                'full_name' => 'System Admin',
+                'gender' => 'M',
+                'birth_date' => '1990-01-01',
+                'birth_place' => 'Dar es Salaam',
+                'marital_status' => 'Hajaoa',
+                'spouse_name' => null,
+                'children_count' => 0,
+                'zone' => 'MURUBOMBO',
+                'phone' => '255744141430',
+                'email' => 'lutufyo28@gmail.com',
+                'password' => Hash::make('admin1234'),
+                'role' => 'admin',
+            ]
+        );
 
     }
 }
