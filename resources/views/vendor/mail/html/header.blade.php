02b3cac4a0e1197513898ae68bcd1709bfa63c36
shadrack <shadrackmussa97@gmail.com>
2026-06-06 00:39:01 +0300

    notification logo

---
+++ resources/views/vendor/mail/html/header.blade.php
@@ -0,0 +1,9 @@
+@props(['url'])
+
+<tr>
+<td class="header">
+<a href="{{ $url }}" style="display: inline-block;">
+<img src="{{ url('assets/logo/logo.png') }}" class="logo" alt="App Logo">
+</a>
+</td>
+</tr>
